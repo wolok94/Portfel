460875c8f249df22cf7df13f27ae0fb9644ed8d6
Kamil Wolak <kamil.wolak@interia.eu>
2023-05-16 10:21:48 +0200

    Added unit test for GetAllExpenses

--- Portfel_Unit_Tests/ExpenseFunctions/Query/GetAllExpensesTest.cs
+++ Portfel_Unit_Tests/ExpenseFunctions/Query/GetAllExpensesTest.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using Moq;
+using Portfel.Application.AutoMapper;
+using Portfel.Application.Contracts.Persistance;
+using Portfel.Application.Functions.ExpenseFunctions.Query.GetAllExpenses;
+using Portfel.Application.UnitTests.Helper;
+using Shouldly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,29 @@ using System.Threading.Tasks;
 
 namespace Portfel.Application.UnitTests.ExpenseFunctions.Query
 {
-    internal class GetAllExpensesTest
+    public class GetAllExpensesTest
     {
+        private readonly IMock<IExpenseRepository> _mockRepository;
+        private readonly IMapper _mapper;
+        public GetAllExpensesTest()
+        {
+            _mockRepository = GetExpenseHelper.GetMock();
+            var configureProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            _mapper = configureProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetAllExpenses_ReturnListOfExpenses()
+        {
+            var handler = new GetAllExpensesQueryHandler(_mockRepository.Object, _mapper);
+
+            var expenses = await handler.Handle(new GetAllExpensesQuery(), CancellationToken.None);
+
+            expenses.ShouldBeOfType(typeof(List<GetAllExpensesViewModel>));
+            expenses.Count.ShouldBe(1);
+        }
     }
 }
