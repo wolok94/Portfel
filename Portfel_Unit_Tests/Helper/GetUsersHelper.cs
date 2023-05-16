5db549520da3f57cb90164ef60b5f2572f009628
Kamil Wolak <kamil.wolak@interia.eu>
2023-05-16 11:21:56 +0200

    Added unit test for adding user

---
+++ Portfel_Unit_Tests/Helper/GetUsersHelper.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using Portfel.Application.Contracts.Persistance;
+using PortfelDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfel.Application.UnitTests.Helper
+{
+    public class GetUsersHelper
+    {
+        public static IMock<IUserRepository> GetMock()
+        {
+            var mock = new Mock<IUserRepository>();
+            var users = GetUsers();
+
+            mock.Setup(x => x.AddAsync(It.IsAny<User>())).ReturnsAsync((User user) =>
+            {
+                users.Add(user);
+                return user;
+            });
+
+            mock.Setup(x => x.GetAll()).ReturnsAsync(users);
+
+            return mock;
+        }
+
+        private static List<User> GetUsers()
+        {
+            return new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Jan",
+                    LastName = "Kowalski"
+                },                new User
+                {
+                    Id = 2,
+                    FirstName = "Stefan",
+                    LastName = "Kowalski"
+                }
+            };
+            
+        }
+    }
+}
---
+++ Portfel_Unit_Tests/UserFunctions/Commands/AddUserTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using Portfel.Application.Contracts.Persistance;
+using Portfel.Application.Functions.UserFunctions.Commands.CreateUser;
+using Portfel.Application.Functions.UserFunctions.Query;
+using Portfel.Application.UnitTests.Helper;
+using PortfelDomain;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfel.Application.UnitTests.UserFunctions.Commands
+{
+    public class AddUserTest
+    {
+        private readonly IMock<IUserRepository> _mockRepository;
+
+        public AddUserTest()
+        {
+            _mockRepository = GetUsersHelper.GetMock();
+        }
+
+        [Fact]
+        public async Task AddAsync_ForValidModel_ReturnUserId()
+        {
+            var handler = new CreateUserCommandHandler(_mockRepository.Object, new Mock<IPasswordHasher<User>>().Object);
+            var usersBeforeCount = (await _mockRepository.Object.GetAll()).Count;
+
+            var response = await handler.Handle(new CreateUserCommand
+            {
+                FirstName = "Robert",
+                LastName = "Kubica",
+                Password = "123456"
+
+            }, CancellationToken.None);
+
+            var allUsers = (await _mockRepository.Object.GetAll()).Count;
+
+            allUsers.ShouldBe(usersBeforeCount + 1);
+            response.ShouldBeOfType(typeof(int));
+        }
+    }
+}
