19847fe830d27bd3c829277cb865053e9c3a463f
Kamil Wolak <kamil.wolak@interia.eu>
2023-05-17 18:04:48 +0200

    Added getting expense by id in application layer

---
+++ Portfel.Application/Functions/ExpenseFunctions/Query/GetExpenseById/GetExpenseByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using PortfelDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfel.Application.Functions.ExpenseFunctions.Query.GetExpenseById
+{
+    public class GetExpenseByIdQuery : IRequest<Expense>
+    {
+        public int ExpenseId { get; set; }
+    }
+}
