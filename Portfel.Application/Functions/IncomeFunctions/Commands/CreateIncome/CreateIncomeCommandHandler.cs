93d8d77082e1e660e9b6fa31fc49f48daefe439e
Kamil Wolak <kamil.wolak@interia.eu>
2023-05-17 09:49:31 +0200

    Added creating income

---
+++ Portfel.Application/Functions/IncomeFunctions/Commands/CreateIncome/CreateIncomeCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using Portfel.Application.Contracts.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfel.Application.Functions.IncomeFunctions.Commands.CreateIncome
+{
+    public class CreateIncomeCommandHandler
+    {
+        private readonly IMock<IIncomeRepository>
+    }
+}
